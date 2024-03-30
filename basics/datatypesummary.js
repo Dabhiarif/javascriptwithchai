345989364f7abd75f1230dea88b933dd01a8662f
Dabhiarif <132182725+Dabhiarif@users.noreply.github.com>
2024-03-30 17:52:59 +0000

    summary

---
+++ basics/datatypesummary.js
@@ -0,0 +1,14 @@
+// Primitive 
+// 7 types:String, Number,Boolean, null,undefined, Symbol,Bigint
+
+// Reference(non primitive)
+//Array ,object, function
+const Marvel =["CaptainAmerica", "ironman", "spidy"];
+let Myobj ={
+    name: "arif",
+    age: "23",
+
+}
+const myfunction = function(){
+    console.log("hello buddy");
+}
