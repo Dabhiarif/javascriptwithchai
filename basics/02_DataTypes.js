9737085bf663ad66d31036b0cca212a1b0d46bd7
Dabhiarif <132182725+Dabhiarif@users.noreply.github.com>
2024-03-30 07:01:10 +0000

    data types

---
+++ basics/02_DataTypes.js
@@ -0,0 +1,19 @@
+"use strict";// treat all js code as newer never version
+
+
+console.log(3+3);
+//  number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol
+
+// object 
+console.log(typeof "arif");
+console.log(typeof null);
+//null is treated as object in javascript
+
+console.log(typeof undefined);
+//undefined is defined undefined in javascript 
