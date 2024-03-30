ef022773d0c752aca131bbaf5934b21a8870e03e
Dabhiarif <132182725+Dabhiarif@users.noreply.github.com>
2024-03-30 07:19:12 +0000

    conversions

---
+++ basics/03_conversion.js
@@ -0,0 +1,10 @@
+let score = "77abc"
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let someNumber = 77
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
