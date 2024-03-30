5ea2b7e5e0f2d2f766144260a327009b87dd8e9e
Dabhiarif <132182725+Dabhiarif@users.noreply.github.com>
2024-03-30 07:07:17 +0000

    datatypes

---
+++ basics/variable.js
@@ -0,0 +1,45 @@
+const accountId = 1710
+let accountEmail = "arif123@gmail.com"
+var accountPassword = "123345"
+accountCity = "jaipur"
+
+//accountId = 2 // this is not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "54321"
+accountCity = "dhg"
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+/*
+always use let for variable
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
